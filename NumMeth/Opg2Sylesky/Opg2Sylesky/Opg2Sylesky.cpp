efc5c2cb1f3fc21193200c8d61f9c001b27519e8
Javakin <danielharaldon@yahoo.com>
2017-02-14 15:59:09 +0100

    jeg kan nu udskrive matricer og vectore

--- NumMeth/Opg2Sylesky/Opg2Sylesky/Opg2Sylesky.cpp
+++ NumMeth/Opg2Sylesky/Opg2Sylesky/Opg2Sylesky.cpp
@@ -5,13 +5,14 @@
 #include <iostream>
 #include <fstream>
 #include <nr3.h>
+#include "utilities.h"
 
 using namespace std;
 
 int main() {
 
 	cout << "==========================\n";
-	cout << "Display the Filip vector \n";
+	cout << "Display the Filip vector  \n";
 	cout << "==========================\n";
 
 	VecDoub xFilip(82); VecDoub yFilip(82);
@@ -31,13 +32,19 @@ int main() {
 	for (int i = 0; i < 40; i++) {
 		Pont >> yPont[i];
 		Pont >> xPont[i];
-		cout << xPont[i] << "\t" << yPont[i] << "\n";
+		//cout << xPont[i] << "\t" << yPont[i] << "\n";
 	}
 
 	// Defiine the equations
-	MatDoub A(3, 40);
-	MatDoub b(1, 40);
+	MatDoub A(40, 3);
+	MatDoub b(40, 1);
 
+	for (int i = 0; i < 40; i++)
+	{
+		A[i][0] = 1;
+		A[i][1] = xPont[i];
+		A[i][2] = xPont[i]*xPont[i];
+	}
 
 	// perform the decompositions
 
@@ -49,7 +56,11 @@ int main() {
 	//calculate the solution
 
 
-
+	// print soluiton
+	util::print(A, "This is a test");
+	
+	util::print(xPont, "xPoint");
+	util::print(yPont, "yPoint");
 
 	system("pause");
 	return 0;
