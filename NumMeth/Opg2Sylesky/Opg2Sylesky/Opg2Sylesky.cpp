44f519c76cb17d0449f5fb800f94f838fd1f2d5d
Javakin <danielharaldon@yahoo.com>
2017-02-14 15:22:37 +0100

    så er vi igang har skrevet lidt pseudo kode

---
+++ NumMeth/Opg2Sylesky/Opg2Sylesky/Opg2Sylesky.cpp
@@ -0,0 +1,57 @@
+// Opg2Sylesky.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <nr3.h>
+
+using namespace std;
+
+int main() {
+
+	cout << "==========================\n";
+	cout << "Display the Filip vector \n";
+	cout << "==========================\n";
+
+	VecDoub xFilip(82); VecDoub yFilip(82);
+	ifstream Filip("FilipData.dat");
+	for (int i = 0; i < 82; i++) {
+		Filip >> yFilip[i];
+		Filip >> xFilip[i];
+		//cout << xFilip[i] << "\t" << yFilip[i] << "\n";
+	}
+
+	cout << "==========================\n";
+	cout << "Display the pontius vector\n";
+	cout << "==========================\n";
+
+	VecDoub xPont(40); VecDoub yPont(40);
+	ifstream Pont("PontiusData.dat");
+	for (int i = 0; i < 40; i++) {
+		Pont >> yPont[i];
+		Pont >> xPont[i];
+		cout << xPont[i] << "\t" << yPont[i] << "\n";
+	}
+
+	// Defiine the equations
+	MatDoub A(3, 40);
+	MatDoub b(1, 40);
+
+
+	// perform the decompositions
+
+	// using choldc 
+
+	// using ludcmp
+	
+
+	//calculate the solution
+
+
+
+
+	system("pause");
+	return 0;
+}
+
