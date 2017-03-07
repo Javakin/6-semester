9019090015c7d0e8cc67d69b548533040e4b05b5
Javakin <danielharaldson@yahoo.com>
2017-03-07 13:47:04 +0100

    project created

---
+++ NumMeth/Opg4-SVD/Opg4-SVD/Opg4-SVD.cpp
@@ -0,0 +1,16 @@
+// Opg4-SVD.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+
+	system("pause");
+    return 0;
+}
+
