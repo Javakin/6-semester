d95e0c05fc33972567145725f9fcd559efa9ed9d
Javakin <danielharaldson@yahoo.com>
2017-04-27 07:59:40 +0200

    f

--- NumMeth/DisplayClass/DisplayClass/TableHandle.cpp
+++ NumMeth/DisplayClass/DisplayClass/TableHandle.cpp
@@ -112,6 +112,18 @@ void TableHandle::print(string description)
 	print();
 }
 
+void TableHandle::exportTable(string type)
+{
+	ofstream myfile;
+	myfile.open("example.csv");
+	myfile << "This is the first cell in the first column.\n";
+	myfile << "a,b,c,\n";
+	myfile << "c,s,v,\n";
+	myfile << "1,2,3.456\n";
+	myfile << "semi;colon";
+	myfile.close();
+}
+
 TableHandle::~TableHandle()
 {
 }
