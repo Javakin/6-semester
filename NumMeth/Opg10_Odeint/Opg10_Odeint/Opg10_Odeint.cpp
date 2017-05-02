abdefee1a196a4a93dba934f1bd56d8340028c4e
Javakin <danielharaldson@yahoo.com>
2017-05-02 08:43:48 +0200

    så er projektet startet

---
+++ NumMeth/Opg10_Odeint/Opg10_Odeint/Opg10_Odeint.cpp
@@ -0,0 +1,18 @@
+// Opg10_Odeint.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "nr3.h"
+#include "odeint.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "gogogo\n";
+
+	system("pause");
+    return 0;
+}
+
